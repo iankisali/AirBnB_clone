69211f3a981f728843aec27c58def4741b855fb0
iankisali <iankisali@gmail.com>
2022-11-23 08:39:51 -0800

    amenity class inheriting fromm BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creates a Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseMode):
+    """Class for managing amenity objects"""
+    name = ""
