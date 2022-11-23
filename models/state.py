56026d04f3afba4ff1733f2c7d37db8ec176ab67
iankisali <iankisali@gmail.com>
2022-11-23 08:39:06 -0800

    state class inheriting from BaseModel

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class state inheriting from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class state inheriting from BaseModel"""
+    name = ""
